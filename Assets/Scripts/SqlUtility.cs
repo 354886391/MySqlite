3b820af4c1e7f1a0f3b290dd108f985812990763
Ancolo <Ancolo@ANCOLO>
2020-12-12 01:12:12 +0800

    01

--- Assets/Scripts/SqlUtility.cs
+++ Assets/Scripts/SqlUtility.cs
@@ -3,7 +3,7 @@ using System;
 using System.Text;
 using UnityEngine;
 
-public class SqlUtility : MonoBehaviour
+public static class SqlUtility
 {
     public static string FilePath
     {
@@ -20,11 +20,11 @@ public class SqlUtility : MonoBehaviour
         }
     }
 
-    private SqliteConnection sqlConnection;
-    private SqliteCommand sqlCommand;
-    private SqliteDataReader sqlReader;
+    private static SqliteConnection sqlConnection;
+    private static SqliteCommand sqlCommand;
+    private static SqliteDataReader sqlReader;
 
-    private void OpenDb()
+    private static void OpenDb()
     {
         if (sqlConnection == null)
         {
@@ -40,7 +40,7 @@ public class SqlUtility : MonoBehaviour
         }
     }
 
-    private void CloseDb()
+    private static void CloseDb()
     {
         if (sqlReader != null)
         {
@@ -59,34 +59,47 @@ public class SqlUtility : MonoBehaviour
         }
     }
 
-    private string CreateTableText<T>(string tablename)
+    public static string CreateTableText<T>(string tablename)
     {
         var length = RefUtility.GetPropertiesLength<T>();
-        StringBuilder builder = new StringBuilder(string.Format(@"CREATE TABLE IF NOT EXISTS {0}(", tablename));
+        StringBuilder builder = new StringBuilder(string.Format(@"CREATE TABLE IF NOT EXISTS {0} (", tablename));
         for (int i = 0; i < length; i++)
         {
-            builder.AppendFormat("{0} {1}{2}", RefUtility.GetPropertyName(i), RefUtility.GetSqliteType(i), i < length - 1 ? ", " : ");");
+            builder.AppendFormat("{0} {1}{2}", RefUtility.GetPropertyName(i), RefUtility.GetSqliteType(i), i < length - 1 ? ", " : ")");
         }
         return builder.ToString();
     }
 
-    private string InsertTableText<T>(string tablename, T t)
+    public static string InsertTableText<T>(string tablename, T t)
     {
         var length = RefUtility.GetPropertiesLength<T>();
-        var builder = new StringBuilder(string.Format(@"INSERT INTO {0} VALUES(", tablename));
+        var builder = new StringBuilder(string.Format(@"INSERT INTO {0} VALUES (", tablename));
         for (var i = 0; i < length; i++)
         {
-            builder.AppendFormat("\'{0}\'{1}", RefUtility.GetPropertyValue(i, t), i < length - 1 ? ", " : ");");
+            builder.AppendFormat("\'{0}\'{1}", RefUtility.GetPropertyValue(i, t), i < length - 1 ? ", " : ")");
         }
         return builder.ToString();
     }
 
-    private string SelectTableText(string tablename, int index)
+    public static string SelectTableText(string tablename, int index)
     {
-        return string.Format(@"SELECT * FROM {0} LIMIT 1 OFFSET {1};", tablename, index);
+        return string.Format(@"SELECT * FROM {0} LIMIT 1 OFFSET {1}", tablename, index);
     }
 
-    public bool Save<T>(string tablename, T t)
+    private const string _updateCmd =
+        @"UPDATE Player SET Bet=@bet, Rate=@rate, Score=@score, Board=@board, JackpotType=@jackpotType, JackpotValue=@jackpotValue WHERE Id=@id";
+    public static string UpdateTableText<T>(string tablename, T t, int index)
+    {
+        var length = RefUtility.GetPropertiesLength<T>();
+        var builder = new StringBuilder(string.Format(@"UPDATE {0} SET ", tablename));
+        for (var i = 0; i < length; i++)
+        {
+            builder.AppendFormat("{0}=\'{1}\'{2}", RefUtility.GetPropertyName(i), RefUtility.GetPropertyValue(i, t), i < length - 1 ? ", " : " WHERE ROWID=" + (index + 1));
+        }
+        return builder.ToString();
+    }
+
+    public static bool Save<T>(string tablename, T t)
     {
         try
         {
@@ -101,7 +114,14 @@ public class SqlUtility : MonoBehaviour
         }
     }
 
-    public void Load<T>(string tablename, T t, int index) where T : class, new()
+    /// <summary>
+    /// index [0, count-1]
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="tablename"></param>
+    /// <param name="t"></param>
+    /// <param name="index"></param>
+    public static void Load<T>(string tablename, T t, int index) where T : class, new()
     {
         try
         {
@@ -115,7 +135,7 @@ public class SqlUtility : MonoBehaviour
                     for (int i = 0; i < count; i++)
                     {
                         RefUtility.SetPropertiesValue(i, t, sqlReader.GetValue(i));
-                        Debug.LogFormat("{0} Value: {1}", i, sqlReader.GetValue(i));
+                        //Debug.LogFormat("{0} Value: {1}", i, sqlReader.GetValue(i));
                     }
                 }
             }
@@ -127,23 +147,31 @@ public class SqlUtility : MonoBehaviour
         }
     }
 
-    private void Start()
+    /// <summary>
+    /// rowid [1, count-1]
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="tablename"></param>
+    /// <param name="t"></param>
+    /// <param name="index"></param>
+    /// <returns></returns>
+    public static bool Update<T>(string tablename, T t, int index)
     {
-        Book book = new Book() { Guid = "ISBN9787111075752", Name = "设计模式-可复用面向对象软件的基础", Price = 35.89m, Press = "机械工业出版社", Classify = (byte)BookType.Education, IsEBook = false };
-        Debug.Log(CreateTableText<Book>("data"));
-        Debug.Log(InsertTableText<Book>("data", book));
-        Debug.Log(SelectTableText("data", 0));
-        Save("data", book);
-        Load("data", book, 0);
-        PrintBook(book);
+        try
+        {
+            OpenDb();
+            sqlCommand.CommandText = UpdateTableText(tablename, t, index);
+            return sqlCommand.ExecuteNonQuery() == 1;
+
+        }
+        catch (Exception e)
+        {
+            Debug.LogWarning(e.Message + "\n" + e.Source + "\n" + e.StackTrace + "\n" + e.Data);
+            return false;
+        }
     }
 
-    private void OnDestroy()
-    {
-        CloseDb();
-    }
-
-    private void PrintBook(Book book)
+    public static void PrintBook(Book book)
     {
         Debug.LogFormat(" Guid = {0}, Name = {1}, Price = {2}, Press = {3}, Classify = {4}, IsEBook = {5} ", book.Guid, book.Name, book.Price, book.Press, book.Classify, book.IsEBook);
     }
---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+
+    private void Start()
+    {
+        Book book = new Book() { Guid = "ISBN9787111075752", Name = "设计模式-可复用面向对象软件的基础", Price = 35.89m, Press = "机械工业出版社", Classify = (byte)BookType.Education, IsEBook = false };
+        Debug.Log(SqlUtility.CreateTableText<Book>("data"));
+        Debug.Log(SqlUtility.InsertTableText<Book>("data", book));
+        Debug.Log(SqlUtility.SelectTableText("data", 0));
+        Debug.Log(SqlUtility.UpdateTableText("data", book, 0));
+        SqlUtility.Save("data", book);
+        SqlUtility.Load("data", book, 5);
+        SqlUtility.PrintBook(book);
+        book.Price = 99.9m;
+        SqlUtility.Update("data", book, 5);
+        SqlUtility.Load("data", book, 5);
+        SqlUtility.PrintBook(book);
+    }
+}
